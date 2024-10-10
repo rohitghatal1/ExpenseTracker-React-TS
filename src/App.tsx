b19ed4214a5242cf2603fdec5add0a3a0bd01676
rohitghatal1 <rohitghatal@gmail.com>
2024-10-10 20:44:56 +0545

    added dashboard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
 
-import './App.css';
+import "./App.css";
+import Dashboard from "./Components/Dashboard";
 
 function App() {
   return (
-    <div>
-    </div>
+    <>
+      <Dashboard />
+    </>
   );
 }
 
---
+++ src/Components/Dashboard.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../CSS/dashboard.css'
+
+const Dashboard:React.FC = () => {
+    return(
+        <>
+        <div className='div1'>
+            <p>This is dash board</p>
+        </div>
+        <div className="div2">
+            <p>This is lower lavel</p>
+        </div>
+        </>
+    )
+}
+
+export default Dashboard
