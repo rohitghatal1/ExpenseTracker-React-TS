a380f21667cff03ebb92cead926abb7429a128de
rohitghatal1 <rohitghatal@gmail.com>
2024-10-11 20:25:18 +0545

    installed react router dom

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React from "react";
+import {NavLink} from 'react-router-dom'
 
-const Navbar:React.FC = () => {
-    return(
-        <>
-        </>
-    )
-}
+const Navbar: React.FC = () => {
+  return (
+    <aside className="navbarContainer">
+      <nav className="navbar">
+        <ul className="navbarItems">
+            <li>
+                <NavLink exact to="/" activeClassName="activeLink" className="navLink">Home</NavLink>
+            </li>
+        </ul>
+      </nav>
+    </aside>
+  );
+};
 
-export default Navbar
+export default Navbar;
