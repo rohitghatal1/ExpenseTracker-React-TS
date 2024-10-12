42ddc3e0f7948dfefb1bd74de3cf83a3d5ec8489
rohitghatal1 <rohitghatal@gmail.com>
2024-10-12 21:22:05 +0545

    imported files in main app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 
 import "./App.css";
-import Dashboard from "./Components/Dashboard";
+// import Dashboard from "./Components/Dashboard";
 import Navbar from "./Components/Navbar";
+import Expenses from "./Components/Expenses";
+import Reports from "./Components/Reports";
 
 function App() {
   return (
     <>
       {/* <Dashboard /> */}
       <Navbar/>
+      <Expenses/>
+      <Reports/>
     </>
   );
 }
