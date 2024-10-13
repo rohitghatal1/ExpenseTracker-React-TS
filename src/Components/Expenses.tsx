47ea1963e2318ccd2dff1585344ab7d5526d12fa
rohitghatal1 <rohitghatal@gmail.com>
2024-10-13 20:52:53 +0545

    added expense modal

--- src/Components/Expenses.tsx
+++ src/Components/Expenses.tsx
@@ -10,6 +10,18 @@ const Expenses: React.FC = () => {
                     <span>Rs 28500</span>
                 </div>
                 <section className="newExpenseSection">
+                    <div className="addExpenseModal">
+                        <input type="text" placeholder='Expense Title'/>
+                        <input type="text" placeholder='Amount'/>
+                        <select name="" id="">
+                            <option value="">Grocery</option>
+                            <option value="">Grocery</option>
+                            <option value="">Grocery</option>
+                            <option value="">Grocery</option>
+                        </select>
+                        <input type="date" name="" id="" placeholder='Date'/>
+                        <textarea name="" id="" placeholder='Addtional notes if any.'></textarea>
+                    </div>
                     <div className="addNewExpense">
                         <button className='addExpensebtn'><i className="fa-solid fa-plus"></i> Add Expense</button>
                     </div>
