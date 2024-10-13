f7884bd16469cd737f6055ce8f4b54fba2169813
rohitghatal1 <rohitghatal@gmail.com>
2024-10-13 11:48:28 +0545

    added reports component with styling

--- src/Components/Reports.tsx
+++ src/Components/Reports.tsx
@@ -3,6 +3,11 @@ import React from 'react'
 const Reports : React.FC = () => {
     return (
         <>
+        <div className="reportComponent">
+            <div className="reportContainer">
+                
+            </div>
+        </div>
         </>
     )
 }
