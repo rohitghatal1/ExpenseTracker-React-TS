c271d9096193401a2cc6e0ed71fa39a6d6b4df76
rohitghatal1 <rohitghatal@gmail.com>
2024-10-16 20:32:22 +0545

    added logo in navbar

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 // import { NavLink } from 'react-router-dom';
 import '../CSS/navbar.css'; // Ensure this CSS file exists and is properly styled
+import logo from '../assets/logo/trackfunds.png'
 
 const Navbar: React.FC = () => {
   return (
     <aside className="sidebar">
       <nav className='navbar'>
+        <div className="logo">
+          <img src={logo} alt="" />
+        </div>
       </nav>
     </aside>
   );
