620ba410fbc596a7e3e632df6042e138c5129fb6
rohitghatal1 <rohitghatal@gmail.com>
2024-10-18 12:52:11 +0545

    improved styles for expense details

--- src/Components/Expenses.tsx
+++ src/Components/Expenses.tsx
@@ -5,15 +5,15 @@ const Expenses: React.FC = () => {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const [isAddButtonVisible, setIsAddButtonVisible] = useState<boolean>(true);
 
-  const openNewExpenseModal = ():void => {
+  const openNewExpenseModal = (): void => {
     setIsModalOpen(true);
     setIsAddButtonVisible(false);
-  }
+  };
 
-  const closeAddExpenseModal = ():void => {
+  const closeAddExpenseModal = (): void => {
     setIsModalOpen(false);
     setIsAddButtonVisible(true);
-  }
+  };
   return (
     <div>
       <div className="expenseComponent">
@@ -23,66 +23,83 @@ const Expenses: React.FC = () => {
         </div>
 
         <section className="newExpenseSection">
-          {isModalOpen && <div className="addExpenseModal">
-            <form className="addExpenseForm">
-              <div className="formData">
-                <div className="expenseElement">
-                  <label htmlFor="title">Title</label>
-                  <input type="text" placeholder="Expense description" />
-                </div>
+          {isModalOpen && (
+            <div className="addExpenseModal">
+              <form className="addExpenseForm">
+                <div className="formData">
+                  <div className="expenseElement">
+                    <label htmlFor="title">Title</label>
+                    <input type="text" placeholder="Expense description" />
+                  </div>
 
-                <div className="expenseElement">
-                  <label htmlFor="amount">Amount</label>
-                  <input type="text" placeholder="Amount" />
-                </div>
+                  <div className="expenseElement">
+                    <label htmlFor="amount">Amount</label>
+                    <input type="text" placeholder="Amount" />
+                  </div>
 
-                <div className="expenseElement">
-                  <label htmlFor="category">Category</label>
-                  <select name="" id="">
-                    <option value="">Grocery</option>
-                    <option value="">Food</option>
-                    <option value="">Stationery</option>
-                    <option value="">Fruits</option>
-                  </select>
-                </div>
+                  <div className="expenseElement">
+                    <label htmlFor="category">Category</label>
+                    <select name="" id="">
+                      <option value="">Grocery</option>
+                      <option value="">Food</option>
+                      <option value="">Stationery</option>
+                      <option value="">Fruits</option>
+                    </select>
+                  </div>
 
-                <div className="expenseElement">
-                  <label htmlFor="date">Expense Date</label>
-                  <input type="date" name="" id="" placeholder="Date" />
-                </div>
+                  <div className="expenseElement">
+                    <label htmlFor="date">Expense Date</label>
+                    <input type="date" name="" id="" placeholder="Date" />
+                  </div>
 
-                <div className="expenseElement">
-                  <label htmlFor="notes">Additonal Note</label>
-                  <textarea
-                    name=""
-                    id=""
-                    placeholder="Addtional notes if any."
-                  ></textarea>
+                  <div className="expenseElement">
+                    <label htmlFor="notes">Additonal Note</label>
+                    <textarea
+                      name=""
+                      id=""
+                      placeholder="Addtional notes if any."
+                    ></textarea>
+                  </div>
                 </div>
-              </div>
-              <div className="confirmCancel">
-                <button className="submitBtn"><i className="fas fa-check"></i> Confirm</button>
-                <button className="cancelBtn" onClick={closeAddExpenseModal}><i className="fas fa-times"></i> Cancel</button>
-              </div>
-            </form>
-          </div>}
+                <div className="confirmCancel">
+                  <button className="submitBtn">
+                    <i className="fas fa-check"></i> Confirm
+                  </button>
+                  <button className="cancelBtn" onClick={closeAddExpenseModal}>
+                    <i className="fas fa-times"></i> Cancel
+                  </button>
+                </div>
+              </form>
+            </div>
+          )}
 
-          {isAddButtonVisible && <div className="addNewExpense">
-            <button className="addExpensebtn" onClick={openNewExpenseModal}>
-              <i className="fa-solid fa-plus"></i> Add Expense
-            </button>
-          </div>}
+          {isAddButtonVisible && (
+            <div className="addNewExpense">
+              <button className="addExpensebtn" onClick={openNewExpenseModal}>
+                <i className="fa-solid fa-plus"></i> Add Expense
+              </button>
+            </div>
+          )}
         </section>
         <div className="expenseContainer">
           <div className="expenseItemsContainer">
             <div className="expenseItem">
               <div className="expenseDetails">
-              <span className="expenseName">Chaumin</span>
-              <span className="category">Food</span>
+                <div className="expenseCategory">
+                  <span className="expenseName">Chaumin</span>
+                  <span className="category">Food</span>
+                </div>
+                <div className="expenseAmount">
+                  <span>Rs 60</span>
+                </div>
               </div>
               <div className="deleteUpdateExpense">
-                <button className="updateBtn"><i className="fas fa-edit"></i> Update</button>
-                <button className="deleteBtn"><i className="fa-solid fa-trash"></i> Delete</button>
+                <button className="updateBtn">
+                  <i className="fas fa-edit"></i> Update
+                </button>
+                <button className="deleteBtn">
+                  <i className="fa-solid fa-trash"></i> Delete
+                </button>
               </div>
             </div>
           </div>
