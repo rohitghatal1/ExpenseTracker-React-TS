43ff773f26b00001640aa684b282418eebeb0c2a
rohitghatal1 <rohitghatal@gmail.com>
2024-10-19 14:10:50 +0545

    added use state to show the active link

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,38 +1,63 @@
-import React from 'react';
+import React, { useState } from "react";
 // import { NavLink } from 'react-router-dom';
-import { Link } from 'react-router-dom';
-import '../CSS/navbar.css'; // Ensure this CSS file exists and is properly styled
-import logo from '../assets/logo/trackfunds.png'
+import { Link } from "react-router-dom";
+import "../CSS/navbar.css"; // Ensure this CSS file exists and is properly styled
+import logo from "../assets/logo/trackfunds.png";
 
 interface NavbarProps {
   toggleTheme: () => void;
   theme: string;
 }
 
-const Navbar: React.FC<NavbarProps> = ({toggleTheme, theme}) => {
+const Navbar: React.FC<NavbarProps> = ({ toggleTheme, theme }) => {
+  const [activeLink, setActiveLink] = useState<string>("dashboard");
+
   return (
     <aside className="sidebar">
-      <nav className='navbar'>
+      <nav className="navbar">
         <div className="logo">
           <img src={logo} alt="" />
         </div>
         <div className="navigations">
           <ul className="navbarList">
-            <li className='navLink activeLink'>
-              <Link to={"/"}><i className="fa-solid fa-table-columns"></i> Dashboard</Link>
+            <Link
+              to="/"
+              className={`navLink ${
+                activeLink === "dashboard" ? "activeLink" : ""
+              }`}
+              onClick={() => setActiveLink("dashboard")}
+            >
+              <li className="linkItem">
+                <i className="fa-solid fa-table-columns"></i> Dashboard
               </li>
-            <li className='navLink'>
-              <Link to={"/expenses"}><i className="fas fa-wallet"></i> Expenses</Link>
-            </li>
-            <li className='navLink'>
-              <Link to={"/reports"}><i className="fas fa-file-alt"></i> Reports</Link> 
-            </li>
+            </Link>
+            <Link
+              to="/expenses"
+              className={`navLink ${
+                activeLink === "expenses" ? "activeLink" : ""
+              }`}
+              onClick={() => setActiveLink("expenses")}
+            >
+              <li className="linkItem">
+                <i className="fas fa-wallet"></i> Expenses
+              </li>
+            </Link>
+            <Link
+              to="/reports"
+              className={`navLink ${
+                activeLink === "reports" ? "activeLink" : ""
+              }`}
+              onClick={() => setActiveLink("reports")}
+            >
+              <li className="linkItem">
+                <i className="fas fa-file-alt"></i> Reports
+              </li>
+            </Link>
           </ul>
         </div>
-        <div className="settingUser">
-          <div className="userAccount">
 
-          </div>
+        <div className="settingUser">
+          <div className="userAccount"></div>
           <div className="modeSwitch">
             <button onClick={toggleTheme}>
               {theme === "root" && "Switch to Dark Mode"}
