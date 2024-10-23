52feded2036b95d196e5b7f840bd1140ca235a09
rohitghatal1 <rohitghatal@gmail.com>
2024-10-23 20:30:48 +0545

    added dashboard component

--- src/Components/Dashboard.tsx
+++ src/Components/Dashboard.tsx
@@ -3,16 +3,11 @@ import '../CSS/dashboard.css'
 
 const Dashboard:React.FC = () => {
     return(
-        <>
-        <div className='div1'>
-            <p>This is dash board</p>
+        <div className='dashboardComponent'>
+            <div className="dashboardContainer">
+                <h2>This is Dashboard</h2>
+            </div>
         </div>
-        <div className="div2">
-            <p>This is lower lavel</p>
-            <button className='btn1'>Add expense</button>
-            <button className='btn2'>Cancel</button>
-        </div>
-        </>
     )
 }
 
