f0942c7e711a2e3a925686cbf7ec2e93294dc17f
rohitghatal1 <rohitghatal@gmail.com>
2024-10-23 15:01:17 +0545

    added model for users

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,4 +13,17 @@ app.use(cors());
 app.use(express.json());
 
 // connecting to MongoDB
-mongoose.connect(process.env.MONGO_URI || '', { useNewUrlParser: true, useUnifiedTopology: true})
+const mongoURI = process.env.MONGO_URI;
+
+if(!mongoURI) {
+    console.error('MongoDB URI is not defined in environment varibales');
+    process.exit(1);
+}
+
+mongoose.connect(mongoURI)
+.then(() => console.log('MongoDB Connected'))
+.catch(err => console.error('MongoDB connection error:', err));
+
+app.listen(PORT, ()=> {
+    console.log(`Server running on port ${PORT}`)
+})
