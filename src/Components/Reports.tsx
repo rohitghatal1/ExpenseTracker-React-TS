893df1ae46009cfd57d8535aca2fafea151bde63
rohitghatal1 <rohitghatal@gmail.com>
2024-10-25 20:49:43 +0545

    added reports component

--- src/Components/Reports.tsx
+++ src/Components/Reports.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import '../CSS/reports.css'
 
 const Reports : React.FC = () => {
     return (
         <>
         <div className="reportComponent">
             <div className="reportContainer">
-                
+                <h2>This is Reports Tab</h2>
             </div>
         </div>
         </>
