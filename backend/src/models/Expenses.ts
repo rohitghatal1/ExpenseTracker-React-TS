26120e975d1984d6d499048c8c97af2e048ca3ad
rohitghatal1 <rohitghatal@gmail.com>
2024-10-27 20:26:48 +0545

    added schema for expense data

--- backend/src/models/Expenses.ts
+++ backend/src/models/Expenses.ts
@@ -7,3 +7,13 @@ interface IExpense extends Document {
     date: Date;
     notes?: string;
 }
+
+const expenseSchema = new Schema<IExpense>({
+    title: {type: String, required: true},
+    amount: { type: Number, required: true },
+    category: { type: String, required: true },
+    date: { type: Date, required: true },
+    notes: { type: String}
+})
+
+export default model<IExpense>("Expense", expenseSchema);
