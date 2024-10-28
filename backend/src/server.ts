098d8b3ff2cae3506e601b0c42612a6fcb369c86
rohitghatal1 <rohitghatal@gmail.com>
2024-10-28 13:32:34 +0545

    added routes in server and connection string

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,12 +2,14 @@ import express, { Request, Response } from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+import exppenseRoutes from './routes/expenses'
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use("/api", exppenseRoutes);
 
 mongoose.connect(process.env.Mongo_URI || "")
 .then(()=> console.log("MongoDB connected"))
