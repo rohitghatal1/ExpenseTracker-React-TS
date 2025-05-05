d7953c79d76b5c2fd885ab763a5f55a47b10af7d
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2025-05-05 22:39:16 +0545

    updated

--- src/Components/MainScreen.tsx
+++ src/Components/MainScreen.tsx
@@ -2,6 +2,12 @@ const MainScreen = () => {
   return (
     <div>
       <h2>MainScreen</h2>
+      <div>
+        <div>
+          <h2>Sidebar</h2>
+        </div>
+        <div>header</div>
+      </div>
     </div>
   );
 };
